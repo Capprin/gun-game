170d32a39fa167c75b5205d216770766094d967b
Capprin Bass <capprin@gmail.com>
2020-07-10 13:56:21 -0600

    Project Setup

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed; // units per update
+
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        if (movement.magnitude > 1) {
+            movement.Normalize();
+        }
+
+        body.MovePosition(body.position + speed * movement * Time.fixedDeltaTime);
+    }
+}
