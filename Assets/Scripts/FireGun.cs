6d3dc413960f27d1e4a138694e108be2808e388c
Vasa <vasilylyapunov@gmail.com>
2020-07-10 15:56:07 -0600

    bullets new appear when on fire button

--- Assets/Scripts/FireGun.cs
+++ Assets/Scripts/FireGun.cs
@@ -13,6 +13,12 @@ public class FireGun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float firing = Input.GetAxis("Fire1");
+
+        // TODO - assuming exactly one gun-tagged object at all times; if the gun is lost we will crash
+        if(firing > 0) {
+            GameObject Gun = GameObject.FindGameObjectsWithTag("Gun")[0];
+            Instantiate((UnityEngine.Object)Resources.Load("Bullet"), new Vector2(Gun.transform.position.x, Gun.transform.position.y), Quaternion.identity);
+        }
     }
 }
