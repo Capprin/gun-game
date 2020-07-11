d228f9c4db7f88508b873c9bde8f64b282ffd092
Vasa <vasilylyapunov@gmail.com>
2020-07-10 21:49:19 -0600

    added a single mod and a mod manager

---
+++ Assets/Scripts/mods/mod.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class mod : MonoBehaviour {
+
+    public abstract void Activate();
+}
---
+++ Assets/Scripts/mods/mod_increaseFireRate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mod_increaseFireRate : mod
+{
+    GameObject gun;
+
+    public override void Activate() {
+        gun = GameObject.FindGameObjectWithTag("Gun");
+        gun.GetComponent<FireGun>().fire_delay_init /= 2;
+    }
+}
