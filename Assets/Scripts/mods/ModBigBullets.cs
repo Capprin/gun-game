d6d204e30b1fc627502990e36e76f07753d6e35b
Capprin Bass <capprin@gmail.com>
2020-07-11 00:56:09 -0600

    Added slow move, big bullets

---
+++ Assets/Scripts/mods/ModBigBullets.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModBigBullets : Mod
+{
+    public override void Activate() {
+        GameObject gun = GameObject.FindGameObjectWithTag("Gun");
+        gun.GetComponent<FireGun>().bullet_scale *= 2;
+    }
+
+    public override string GetName() {
+        return "Big Bullets";
+    }
+}
---
+++ Assets/Scripts/mods/ModSlowMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModSlowMove : Mod
+{
+    public override void Activate() {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<PlayerMovement>().speed *= 0.7f;
+    }
+
+    public override string GetName() {
+        return "Slow Move";
+    }
+}
