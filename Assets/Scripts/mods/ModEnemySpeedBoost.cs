ffe8f2b55c6e54daff7e71487db12522ad60109a
Capprin Bass <capprin@gmail.com>
2020-07-11 13:01:24 -0600

    Added enemy speed boost

---
+++ Assets/Scripts/mods/ModEnemySpeedBoost.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModEnemySpeedBoost : Mod
+{
+    public override void Activate() {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemies) {
+            enemy.GetComponent<BasicEnemy>().speed *= 1.05f;
+        }
+    }
+
+    public override string GetName() {
+        return "Enemy Speed Boost";
+    }
+}
