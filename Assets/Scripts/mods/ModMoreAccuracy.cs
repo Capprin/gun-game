f590f23aab96faefd68bba3d038805e848a790ac
Capprin Bass <capprin@gmail.com>
2020-07-11 13:13:53 -0600

    Added deadshot (95% accuracy)

---
+++ Assets/Scripts/mods/ModMoreAccuracy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModMoreAccuracy : Mod
+{
+    public override void Activate() {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponentInChildren<FireGun>().accuracy = 0.95f;
+    }
+
+    public override string GetName() {
+        return "Deadshot";
+    }
+}
