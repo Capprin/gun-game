8ed7b96fe9bf6f60b65a6c1c948f9e9c1723bf92
Vasa <vasilylyapunov@gmail.com>
2020-07-10 19:41:39 -0600

    bullets now delete when hitting a while and basic camera following implemented

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Wall") {
+            Destroy(gameObject);
+        }
+    }
+}
