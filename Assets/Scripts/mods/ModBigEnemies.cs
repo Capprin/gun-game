9f4cecd9e71985c85d46f201624cef4dbc05cc16
Capprin Bass <capprin@gmail.com>
2020-07-11 13:16:21 -0600

    Added big enemies

---
+++ Assets/Scripts/mods/ModBigEnemies.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModBigEnemies : Mod
+{
+    public override void Activate() {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemies) {
+            enemy.transform.localScale *= 1.5f;
+        }
+    }
+
+    public override string GetName() {
+        return "Bigger Enemies";
+    }
+}
