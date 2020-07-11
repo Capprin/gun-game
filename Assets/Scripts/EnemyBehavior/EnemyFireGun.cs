0842d408fef8928735b64ed4b1885c986eda9882
Capprin Bass <capprin@gmail.com>
2020-07-11 10:57:36 -0600

    Added EnemyBehavior directory; added enemy gun behaviors

---
+++ Assets/Scripts/EnemyBehavior/EnemyFireGun.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFireGun : MonoBehaviour
+{
+
+    public float fire_frequency = 2.0f;
+    public float accuracy = 0.85f;
+    public float bullet_speed = 1000;
+
+    private float current_fire;
+    void Start()
+    {
+        current_fire = fire_frequency;
+    }
+
+    void Update()
+    {
+        // timer for firing
+        if (current_fire > 0) {
+            current_fire -= Time.deltaTime;
+        } else {
+            Fire();
+            current_fire = fire_frequency;
+        }
+    }
+
+    void Fire() {
+        float fireCone = 90f - 90f * accuracy;
+        float dirMod = Random.Range(-fireCone/2f, fireCone/2f);
+
+        GameObject bullet = (GameObject)Instantiate(Resources.Load("Bullet"), transform.position, transform.rotation);
+        bullet.tag = "EnemyBullet";
+        bullet.transform.Rotate(Vector3.forward * dirMod);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(bullet_speed * bullet.transform.right);
+    }
+}
---
+++ Assets/Scripts/EnemyBehavior/EnemyGunRotate.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGunRotate : MonoBehaviour
+{
+
+    public GameObject lookingFor = null;
+
+    private Vector3 setScale = new Vector3(-2, 2, 0);
+
+    void Start()
+    {
+        if (lookingFor == null) {
+            lookingFor = GameObject.Find("Player");
+        }
+        setScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // flip sprite according to local angle
+        bool onRight = transform.eulerAngles.z < 90 || transform.eulerAngles.z > 270;
+        if (!onRight) {
+            Vector3 newScale = setScale;
+            newScale.y *= -1;
+            transform.localScale = newScale;
+        } else {
+            transform.localScale = setScale;
+        }
+
+        // get angle to player
+        Vector2 toOther = lookingFor.transform.position - transform.position;
+        float localOtherAngle = Mathf.Atan2(toOther.y, toOther.x) * Mathf.Rad2Deg;
+        localOtherAngle = (localOtherAngle + 360) % 360;
+
+        // do rotation
+        float delta = localOtherAngle - transform.eulerAngles.z;
+        transform.RotateAround(transform.parent.transform.position,
+                               Vector3.forward,
+                               delta);
+    }
+}
