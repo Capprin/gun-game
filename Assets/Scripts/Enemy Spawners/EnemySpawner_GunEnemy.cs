13648ed9c34cbb8a54f8c4c9d96e901a8e245113
Capprin Bass <capprin@gmail.com>
2020-07-11 11:20:38 -0600

    Reorganized directories; added gun enemy spawner

---
+++ Assets/Scripts/Enemy Spawners/EnemySpawner_GunEnemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner_GunEnemy : EnemySpawner
+{
+    public override int GetPoints() {
+        return 250;
+    }
+    public override string GetName() {
+        return "EnemyGun";
+    }
+}
