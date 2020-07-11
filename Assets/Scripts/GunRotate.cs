e2854db0684d48cf80c55c56b6c7a1bfa19978de
Capprin Bass <capprin@gmail.com>
2020-07-10 21:34:30 -0600

    Added gun rotation

--- Assets/Scripts/GunRotate.cs
+++ Assets/Scripts/GunRotate.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class GunRotate : MonoBehaviour
 {
 
+    private Vector3 setScale = new Vector3(7, 7, 0);
+    void Start() {
+        setScale = transform.localScale;
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         float joyX = Input.GetAxis("Joy X");
         float joyY = Input.GetAxis("Joy Y");
+
+        // flip sprite according to joy x
+        if (joyX < 0) {
+            Vector3 newScale = setScale;
+            newScale.y *= -1;
+            transform.localScale = newScale;
+        } else {
+            transform.localScale = setScale;
+        }
 
         float joyAngle = Mathf.Atan2(-joyY, joyX) * Mathf.Rad2Deg;
         joyAngle = (joyAngle + 360) % 360; //go 0-360 instead of with neg
