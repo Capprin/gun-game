ec2fad77d60da77959145ac62db32ac9e40860d1
Capprin Bass <capprin@gmail.com>
2020-07-10 23:38:41 -0600

    Updated naming for first example mod

---
+++ Assets/Scripts/mods/ModIncreaseFireRate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModIncreaseFireRate : Mod
+{
+    public override void Activate() {
+        GameObject gun = GameObject.FindGameObjectWithTag("Gun");
+        if (gun.GetComponent<FireGun>().fire_delay_init >= 2) {
+            gun.GetComponent<FireGun>().fire_delay_init /= 2;
+        }
+    }
+
+    public override string GetName() {
+        return "Increased Fire Rate";
+    }
+}
